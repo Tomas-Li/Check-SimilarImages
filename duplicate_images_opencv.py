c623cbd32219a713036a6d7ab062ae135d3b6467
Tomas Li <tomas.m.l.001@gmail.com>
2021-11-11 18:47:24 -0300

    Everything working but couldnt use class variables, neither the autoremove option

--- duplicate_images_opencv.py
+++ duplicate_images_opencv.py
@@ -11,6 +11,8 @@ Program for a fast comparation of similar features between images in a given pat
 
 #More complete version than the PIL byte by byte comparation option. This one will recognize not only if two images are the same (this implicitly  means that they must have the same size), but if they are similar based on special algorithms of OpenCV library (filters, resizes and cuts should be recognized by the code!)
 
+#TODO:
+#!Autoremove is not implemented (but defined) because of all the problems that imply passing the path at every instance. Can do it easily if I pass another list with complete paths to ImageChecker, but I dont like that idea...
 
 import os
 import cv2
@@ -22,14 +24,17 @@ from sys import argv
 
 class ImageChecker():
     #Needed variables in all instances
-    SIMILARITY_RATIO=None #!Roto
-    MINIMUM_SIMILARITY=None #!Roto
+    #!SIMILARITY_RATIO=None #!Roto
+    #!MINIMUM_SIMILARITY=None #!Roto
     SIFT = cv2.SIFT_create()
     INDEX_PARAMS = dict(algorithm=0, trees=5)
     SEARCH_PARAMS = dict()
     FLANN = cv2.FlannBasedMatcher(INDEX_PARAMS, SEARCH_PARAMS)#Flann is a faster method provided by cv2 than comparing each descriptor for both images
 
-    def __init__(self, img1, title1):
+    def __init__(self, SR, MS, img1, title1):
+        #Have to make these variables because class variables or global variables weren't working with multiprocesing
+        self.SIMILARITY_RATIO = SR
+        self.MINIMUM_SIMILARITY = MS
         #Instance of the main image and it's title (this image is the one that will be compared in series)
         self.img1 = img1
         self.title1 = title1
@@ -56,7 +61,8 @@ class ImageChecker():
         matches = self.FLANN.knnMatch(desc_1, desc_2, k=2)
         good_points = []
         for m,n in matches:
-            if m.distance < ImageChecker.SIMILARITY_RATIO * n.distance:
+            #!if m.distance < ImageChecker.SIMILARITY_RATIO * n.distance:
+            if m.distance < self.SIMILARITY_RATIO * n.distance:
                 good_points.append(m)
 
         if len(kp_1) < len(kp_2):
@@ -64,7 +70,8 @@ class ImageChecker():
         else:
             similarity = int(len(good_points)/len(kp_2)*100)
 
-        if ImageChecker.MINIMUM_SIMILARITY <= similarity:
+        #!if ImageChecker.MINIMUM_SIMILARITY <= similarity:
+        if self.MINIMUM_SIMILARITY <= similarity:
             return f"{self.title1} | {title2} : {similarity}%\n"
 
 
@@ -108,33 +115,37 @@ def settingConfiguration(config:configparser.ConfigParser):
         recursive = False
     
     try:
-        ImageChecker.SIMILARITY_RATIO = config['Options'].getfloat('similarity_ratio')
+        #!ImageChecker.SIMILARITY_RATIO = config['Options'].getfloat('similarity_ratio')
+        SIMILARITY_RATIO = config['Options'].getfloat('similarity_ratio')
     except ValueError:
         print(f"ValueError in ConfigParser -> SIMILARITY_RATIO is not a valid numerical value!")
         print("The code will proceed with the default fallback value:")
-        ImageChecker.SIMILARITY_RATIO = 0.6
+        #!ImageChecker.SIMILARITY_RATIO = 0.6
+        SIMILARITY_RATIO = 0.6
 
     try:
-        ImageChecker.MINIMUM_SIMILARITY = config['Options'].getint('minimum_similarity')
+        #!ImageChecker.MINIMUM_SIMILARITY = config['Options'].getint('minimum_similarity')
+        MINIMUM_SIMILARITY = config['Options'].getint('minimum_similarity')
     except ValueError:
         print(f"ValueError in ConfigParser -> MINIMUM_SIMILARITY is not a valid numerical value!")
         print("The code will proceed with the default fallback value:")
-        ImageChecker.MINIMUM_SIMILARITY = 50
+        #!ImageChecker.MINIMUM_SIMILARITY = 50
+        MINIMUM_SIMILARITY = 50
     
-    return path, autoremove, recursive
+    return SIMILARITY_RATIO, MINIMUM_SIMILARITY, path, autoremove, recursive
 
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('ConfigFile.ini')
     try:
-        path, autoremove, recursive = settingConfiguration(config)
+        SR, MS, path, autoremove, recursive = settingConfiguration(config)
     except KeyError:
         aux = argv[0].split(os.sep)
         aux.pop()
         aux = os.sep.join(aux)
         config.read(f'{aux}{os.sep}ConfigFile.ini')      
-        path, autoremove, recursive = settingConfiguration(config)
+        SR, MS, path, autoremove, recursive = settingConfiguration(config)
 
     #Generate lists containing cv2.imread() and titles.
     all_images = []
@@ -164,7 +175,7 @@ if __name__ == '__main__':
         for i in range(len(all_images)):
             img1 = all_images[i]
             title1 = images_names[i]
-            inst = ImageChecker(img1, title1)
+            inst = ImageChecker(SR, MS, img1, title1)
             with concurrent.futures.ProcessPoolExecutor() as executor:
                 processes = executor.map(inst.main,((img2,title2) for img2,title2 in zip(all_images[i+1:],images_names[i+1:])))
                 for result in processes:
@@ -173,6 +184,3 @@ if __name__ == '__main__':
                     except TypeError:
                         pass
 
-
-#!Tengo que hacer testing recursivos
-#!Tengo que hacer testing de autoremove
